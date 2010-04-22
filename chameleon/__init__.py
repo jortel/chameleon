b4f8507a6927ad78e091138f2caf84b1679338a5
jortel <jortel@7190714e-5d6f-4409-a72e-133770e3411d>
2010-04-22 19:36:56 +0000

    Add printing class.

--- chameleon/__init__.py
+++ chameleon/__init__.py
@@ -15,3 +15,117 @@
 # written by: Jeff Ortel ( jortel@redhat.com )
 
 __version__ = 0.2
+
+
+class PrettyPrinter:
+    """ 
+    Pretty printing of a Object object.
+    """
+    
+    @classmethod
+    def indent(cls, n):
+        return '%*s'%(n*2,' ')
+
+    def tostr(self, pyobj, indent=-2):
+        """ get s string representation of pyobj """
+        history = []
+        return self.process(pyobj, history, indent)
+    
+    def process(self, pyobj, h, n=0, nl=False):
+        """ print pyobj using the specified indent (n) and newline (nl). """
+        if pyobj is None:
+            return 'None'
+        if isinstance(pyobj, dict):
+            if pyobj:
+                return self.printDict(pyobj, h, n+2, nl)
+            else:
+                return '<empty>'
+        if isinstance(pyobj, (list,tuple)):
+            if pyobj:
+                return self.printList(pyobj, h, n+2)
+            else:
+                return '<empty>'
+        if isinstance(pyobj, (basestring, bool, int)):
+            return '"%s"' % str(pyobj)
+        if isinstance(pyobj, object):
+            if pyobj.__dict__:
+                return self.printObject(pyobj, h, n+2, nl)
+            else:
+                return '<empty>'
+        return '%s' % self.process(pyobj)
+    
+    def printObject(self, d, h, n, nl=False):
+        """ print complex using the specified indent (n) and newline (nl). """
+        s = []
+        cls = d.__class__
+        if d in h:
+            s.append('(')
+            s.append(cls.__name__)
+            s.append(')')
+            s.append('...')
+            return ''.join(s)
+        h.append(d)
+        if nl:
+            s.append('\n')
+            s.append(self.indent(n))
+        s.append('(')
+        s.append(cls.__name__)
+        s.append(')')
+        s.append('\n')
+        s.append(self.indent(n))
+        s.append('{')
+        for item in d.__dict__.items():
+            if item[0].startswith('_'):
+                continue
+            s.append('\n')
+            s.append(self.indent(n+1))
+            if isinstance(item[1], (list,tuple)):            
+                s.append(str(item[0]))
+                s.append('[]')
+            else:
+                s.append(str(item[0]))
+            s.append(' = ')
+            s.append(self.process(item[1], h, n, True))
+        s.append('\n')
+        s.append(self.indent(n))
+        s.append('}')
+        h.pop()
+        return ''.join(s)
+    
+    def printDict(self, d, h, n, nl=False):
+        """ print complex using the specified indent (n) and newline (nl). """
+        if d in h: return '{}...'
+        h.append(d)
+        s = []
+        if nl:
+            s.append('\n')
+            s.append(self.indent(n))
+        s.append('{')
+        for item in d.items():
+            s.append('\n')
+            s.append(self.indent(n+1))
+            if isinstance(item[1], (list,tuple)):            
+                s.append(str(item[0]))
+                s.append('[]')
+            else:
+                s.append(str(item[0]))
+            s.append(' = ')
+            s.append(self.process(item[1], h, n, True))
+        s.append('\n')
+        s.append(self.indent(n))
+        s.append('}')
+        h.pop()
+        return ''.join(s)
+
+    def printList(self, c, h, n):
+        """ print collection using the specified indent (n) and newline (nl). """
+        if c in h: return '[]...'
+        h.append(c)
+        s = []
+        for item in c:
+            s.append('\n')
+            s.append(self.indent(n))
+            s.append(self.process(item, h, n-2))
+            s.append(',')
+        h.pop()
+        return ''.join(s)
